9ae41feec1c494a24f137850d30839cf219ea709
Ira Burton <ira.burton@gmail.com>
2013-06-07 16:09:13 -0600

    Added Alerts, Almanac, and Astronomy features

--- bin/weatherunderground.dart
+++ bin/weatherunderground.dart
@@ -1,12 +1,17 @@
 import "dart:io";
 import "dart:json";
 import "dart:async";
+import "dart:collection";
 
 class WeatherUnderground {
   const _apiURL= "http://api.wunderground.com/api/";
   String _apiKey;
   String _locQuery;
   HttpClient _client;
+  Map _apiMap = {'conditions': 'current_observation',
+                 'alerts': 'alerts',
+                 'almanac': 'almanac',
+                 'astronomy': 'moon_phase'};
   
   void setLocationQuery(String locationQuery) {
     _locQuery = locationQuery;
@@ -18,19 +23,37 @@ class WeatherUnderground {
     _client = new HttpClient();
   }
   
+  Future getAlmanac() {
+    return makeAPICall('almanac');
+  }
+  
   Future getConditions() {
+    return makeAPICall('conditions');
+  }
+  
+  Future getAlerts() {
+    return makeAPICall('alerts');
+  }
+  
+  Future getAstronomy() {
+    return makeAPICall('astronomy');
+  }
+  
+  Future makeAPICall(String apiName) {
     Completer completer = new Completer();
     
-    String query = _apiURL + _apiKey + "/conditions/q/" + _locQuery + ".json";
+    String query = _apiURL + _apiKey + "/" + apiName + "/q/" + _locQuery + ".json";
     _client.getUrl(Uri.parse(query))
       .then((HttpClientRequest request) {
         return request.close();
       })
       .then((HttpClientResponse response) {
         response.transform(new StringDecoder()).toList().then((data) {
-          var body = data.join('');
-          var parsedList = parse(body);
-          completer.complete(parsedList['current_observation']);
+          String body = data.join('');
+          LinkedHashMap parsedList = parse(body);
+          String dataKey = parsedList['response']['features'].keys.first; 
+          print(parsedList.toString()); 
+          completer.complete(parsedList[_apiMap[dataKey]]);       
         });        
       });
     return completer.future;
