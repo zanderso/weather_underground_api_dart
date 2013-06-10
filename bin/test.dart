7080dbbc37be0afa4ce37479dc824d4e84a5a2dc
Ira Burton <ira.burton@gmail.com>
2013-06-10 10:44:36 -0600

    Added test file (cleansed of API info) to the project

---
+++ bin/test.dart
@@ -0,0 +1,57 @@
+import "./weatherunderground.dart";
+import "dart:collection";
+
+WeatherUnderground wu;
+
+void main() {
+  print("WeatherUnderground Tests!");
+   
+  // You will need to place your own weather undergound API key in place of the x's
+  wu = new WeatherUnderground("xxxxxxxxxxxxxxxx", "84096");
+
+/*  
+  wu.getConditions().then((LinkedHashMap val) {
+    print(val.toString());
+  } );
+  
+  wu.getAlerts().then((List val) {
+    print(val.toString());
+  } );
+  
+  wu.getAlmanac().then((LinkedHashMap val) {
+    print(val.toString());
+  } );
+  
+  wu.getAstronomy().then((LinkedHashMap val) {
+    print(val.toString());
+  } );  
+  
+  wu.getCurrentHurricane().then((List val) {
+    print(val.toString());
+  } );    
+
+  wu.getForecast().then((LinkedHashMap val) {
+    print(val.toString());
+  } ); 
+  
+  wu.getForecast10Day().then((LinkedHashMap val) {
+    print(val.toString());
+  } );    
+
+  wu.getGeoLookup().then((LinkedHashMap val) {
+    print(val.toString());
+  } );
+    
+  wu.getHistory(new DateTime(2000,1,1)).then((LinkedHashMap val) {
+    print(val.toString());
+  } );
+
+  wu.getHourly().then((List val) {
+    print(val.toString());
+  } );  
+*/
+  wu.getHourly10Day().then((List val) {
+    print(val.toString());
+  } );   
+  
+}
