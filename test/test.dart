8bf254e1389bb1452d54e059ca32ecdb93f15767
iburton <ira.burton@gmail.com>
2013-06-17 20:00:27 -0600

    Added exception catching to test app as an example.

--- test/test.dart
+++ test/test.dart
@@ -13,71 +13,105 @@ void main() {
 
   wu.getConditions().then((var val) {
     print(val.toString());
+  }).catchError((e) {
+    print("An error occurred: ${e.toString()}");
   });
   
   wu.getAlerts().then((var val) {
     print(val.toString());
-  } );
+  }).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });
 
 /*  
   wu.getAlmanac().then((var val) {
     print(val.toString());
-  } );
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });
   
   wu.getAstronomy().then((var val) {
     print(val.toString());
-  } );  
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });  
   
   wu.getCurrentHurricane().then((var val) {
     print(val.toString());
-  } );    
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });    
 
   wu.getForecast().then((var val) {
     print(val.toString());
-  } ); 
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  }); 
   
   wu.getForecast10Day().then((var val) {
     print(val.toString());
-  } );    
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });    
 
   wu.getGeoLookup().then((var val) {
     print(val.toString());
-  } );
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });
     
   wu.getHistory(new DateTime(2000,1,1)).then((var val) {
     print(val.toString());
-  } );
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });
 
   wu.getHourly().then((var val) {
     print(val.toString());
-  } );  
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });  
 
   wu.getHourly10Day().then((var val) {
     print(val.toString());
-  } );   
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });   
 
   wu.getPlanner(7,11,7,15).then((var val) {
     print(val.toString());
-  } );   
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });   
 
   wu.getRawTide().then((var val) {
     print(val.toString());
-  } );  
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });  
   
   wu.getSatellite().then((var val) {
     print(val.toString());
-  } );  
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });  
 
   wu.getTide().then((var val) {
     print(val.toString());
-  } );   
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });   
 
   wu.getWebcams().then((var val) {
     print(val.toString());
-  } );    
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });    
    
   wu.getYesterday().then((var val) {
     print(val.toString());
-  } );    
+  } ).catchError((e) {
+    print("An error occurred: ${e.toString()}");
+  });    
   */
 }
